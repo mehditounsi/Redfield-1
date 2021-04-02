6f721e1c4cfbe880fdbc6b9b3a7b5af9ad0f8a23
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2021-04-02 20:10:26 +0100

    semantic ui

--- redField/server/index.js
+++ redField/server/index.js
@@ -1,13 +1,11 @@
-var express =require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
-var morgan = require('morgan')
+var express = require("express");
+var bodyParser = require("body-parser");
+// var cors = require('cors')
 // const userRouter = require('./Database/routers/UserRouter.js')
 // const DemandRouter = require('./Database/routers/DemandUser.js')
 
 // app.use(userRouter)
 // app.use(DemandRouter)
-
 
 var app = express();
 var PORT = 3000;
@@ -15,10 +13,8 @@ var PORT = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/',express.static('dist/redField'));
+app.use("/", express.static("dist/redField"));
 
-
-
-app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
