de466bea7249c65c7d9e3152591881be6c66b591
Semer-Ben-Salem <semer_ben_salem@yahoo.fr>
2021-04-04 21:17:20 +0100

    fixing : creating and getting users table from dataBase

--- redField/server/Database/models/Comments.js
+++ redField/server/Database/models/Comments.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
 // mongoose.promise = global.promise;
-const demandSchema = new mongoose.Schema({
-  title: String,
+const commentSchema = new mongoose.Schema({
   desc: String,
   createdAt: { type: Date, default: Date.now },
-  comments: [{ id: Schema.Types.ObjectId, ref: "Comment" }],
   users: {
     type: Schema.Types.ObjectId,
     ref: "user",
   },
 });
 
-const demand = mongoose.model("demand", demandSchema);
-module.exports = demand;
+const comment = mongoose.model("comment", commentSchema);
+module.exports = comment;
--- redField/server/index.js
+++ redField/server/index.js
@@ -1,19 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-// var cors = require('cors')
-// const userRouter = require('./Database/routers/UserRouter.js')
-// const DemandRouter = require('./Database/routers/DemandUser.js')
 
-// app.use(userRouter)
-// app.use(DemandRouter)
+const connection = require("./Database/config.js");
+const userRouter = require("./Database/routers/UserRouter.js");
 
 var app = express();
 var PORT = 3000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", express.static("dist/redField"));
+app.use("/", userRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
